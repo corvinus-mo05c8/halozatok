62dc95c02ed6fd78494f2baf5060936d2c9f5a83
Dániel Boros <daniel.boros2@stud.uni-corvinus.hu>
2021-02-11 18:56:14 +0100

    a

--- Hajosteszt/Hajosteszt/Startup.cs
+++ Hajosteszt/Hajosteszt/Startup.cs
@@ -15,11 +15,11 @@ namespace Hajosteszt
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
+        { 
         }
-
+           
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -32,13 +32,14 @@ namespace Hajosteszt
             {
                 endpoints.MapGet("/", async context =>
                 {
+                    app.UseHttpsRedirection();
+                    app.UseDefaultFiles();
+            app.UseStaticFiles();
                     // await context.Response.WriteAsync("Hello World!");
                 });
             });
-
-            app.UseHttpsRedirection();
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
+     
+          
         }
     }
 }
